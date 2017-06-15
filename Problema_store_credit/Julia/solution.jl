b3c7b57339905751bdd33a0f6a584409b229f06b
elsuizo <martin.noblia@openmailbox.org>
2017-06-15 00:23:21 -0300

    update algo

--- Problema_store_credit/Julia/solution.jl
+++ Problema_store_credit/Julia/solution.jl
@@ -14,12 +14,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or (at
 # your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 
 ---------------------------------------------------------------------------=#
@@ -37,7 +37,7 @@ end
 """
 Function for returning the lines of text file
 """
-function read_file(str::AbstractString) 
+function read_file(str::AbstractString)
     file = open(str)
     lines = eachline(file)
     close(file)
